75e7f86359624c17eb33c6394e3afb101ef4d74e
makosaisaac <makosaisaac@gmail.com>
2025-02-27 11:04:26 +0200

    Cleanup package.json and app.component.ts: remove unused dependencies and imports

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,16 +1,10 @@
-import { Location } from '@angular/common';
 import { Component } from '@angular/core';
-import { Title } from '@angular/platform-browser';
-import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { filter, map } from 'rxjs/operators';
-import { ShortSummaryService } from './services/short-summary.service';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
