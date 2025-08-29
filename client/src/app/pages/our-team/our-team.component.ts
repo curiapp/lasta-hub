d3f45a6b9b03db6a14ba0ec8f603dc1ea68a37ee
makosaisaac <makosaisaac@gmail.com>
2025-08-29 03:54:56 +0200

    refactor: simplify OurTeamComponent by removing unused lifecycle methods; update About Us page layout and spacing

--- client/src/app/pages/our-team/our-team.component.ts
+++ client/src/app/pages/our-team/our-team.component.ts
@@ -6,11 +6,4 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './our-team.component.html',
   styleUrls: ['./our-team.component.css']
 })
-export class OurTeamComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class OurTeamComponent {}
