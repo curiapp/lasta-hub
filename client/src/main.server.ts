896e02be5e665d3a77549108554c9687b18849a3
makosaisaac <makosaisaac@gmail.com>
2025-09-24 00:05:48 +0200

    refactor: remove duplicate import of renderApplication in main.server.ts

--- client/src/main.server.ts
+++ client/src/main.server.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { renderApplication } from '@angular/platform-server';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
-import { renderApplication } from '@angular/platform-server';
 
 const bootstrap = () => bootstrapApplication(AppComponent, config);
 
