cbd45d2e2607e0651fe0ed15fb3bba00b1155b7b
Abraham Pelema <apelema@gipf.com.na>
2025-11-06 10:47:28 +0200

    Added more na endpoints

--- server.js/src/routes/need-analysis.ts
+++ server.js/src/routes/need-analysis.ts
@@ -1,8 +1,14 @@
 import { Express } from "express";
-import { eq, or } from "drizzle-orm";
+import { and, eq, or } from "drizzle-orm";
 import { Multer } from "multer";
 
-import { programmes, phaseSteps, programmePhases } from "@/db/schema";
+import {
+  programmes,
+  phaseSteps,
+  programmePhases,
+  phases,
+  programmePhaseSteps,
+} from "@/db/schema";
 import { db } from "@/db/index";
 import {
   apcRecommendSchema,
@@ -22,25 +28,31 @@ export default async (app: Express, upload: Multer) => {
     }
 
     // Save data in db
-    const record = await db.insert(programmes).values(value).returning();
+    const programmeRecord = await db
+      .insert(programmes)
+      .values(value)
+      .returning();
+
     const naId = await db
-      .select({ id: phaseSteps.id })
-      .from(phaseSteps)
-      .where(eq(phaseSteps.name, "Needs Analysis"))
+      .select({ id: phases.id })
+      .from(phases)
+      .where(eq(phases.name, "Needs Analysis"))
       .limit(1);
+
+    //Create programme phase record
     await db.insert(programmePhases).values({
-      programId: record[0].id,
+      programId: programmeRecord[0].id,
       phaseId: naId[0].id,
       status: "In Progress",
     });
 
-    res.send("Need analysis started");
+    res.send({ message: "Need analysis started" });
   });
 
   app.post(
     "/need-analysis/consult",
     upload.array("questionaires"),
-    (req, res) => {
+    async (req, res) => {
       const { error, value } = consultSchema.validate(
         {
           ...req.body,
@@ -56,15 +68,55 @@ export default async (app: Express, upload: Multer) => {
           .send(error.details.map(({ message }) => message));
       }
 
-      console.log(req.files);
-      console.log(req.body);
+      //save files and get url
 
-      return res.send("Consultation step - Not implemented");
+      const [naPhaseStep] = await db
+        .select({ id: phaseSteps.id })
+        .from(phaseSteps)
+        .innerJoin(phases, eq(phaseSteps.phaseId, phases.id))
+        .where(
+          and(
+            eq(phases.name, "Needs Analysis"),
+            eq(phaseSteps.name, "Stakeholders Consultation")
+          )
+        )
+        .limit(1);
+
+      const [programmePhase] = await db
+        .select({ id: programmePhases.id })
+        .from(programmePhases)
+        .where(
+          and(
+            eq(programmePhases.phaseId, naPhaseStep.id),
+            eq(programmePhases.programId, value.programmeId)
+          )
+        )
+        .limit(1);
+
+      //Create programme phase step record for "Stakeholders Consultation"
+      await db.insert(programmePhaseSteps).values({
+        programmePhaseId: programmePhase.id,
+        phaseStepId: naPhaseStep.id,
+        extraData: JSON.stringify({
+          //save file url from each organization
+          organizations: value.organizations,
+          questionaires: [],
+          startDate: value.startDate,
+          endDate: value.endDate,
+        }),
+      });
+
+      return res.send({
+        message: "Data successfully recorded",
+      });
     }
   );
 
-  app.post("/need-analysis/survey", upload.single("file"), (req, res) => {
-    const { error, value } = programmeIdSchema.validate(
+  app.post("/need-analysis/survey", upload.single("file"), async (req, res) => {
+    const {
+      error,
+      value: { programmeId },
+    } = programmeIdSchema.validate(
       {
         ...req.body,
       },
@@ -76,7 +128,39 @@ export default async (app: Express, upload: Multer) => {
       return res.status(400).send(error.details.map(({ message }) => message));
     }
 
-    return res.send("Survey step - Not implemented");
+    //save submitted file
+
+    const [record] = await db
+      .select({
+        id: programmePhaseSteps.id,
+        extraData: programmePhaseSteps.extraData,
+      })
+      .from(programmePhaseSteps)
+      .innerJoin(
+        programmePhases,
+        eq(programmePhaseSteps.programmePhaseId, programmePhases.id)
+      )
+      .where(eq(programmePhases.programId, programmeId))
+      .limit(1);
+
+    if (!record)
+      return res
+        .send({ message: "Programme Need Analysis step not found" })
+        .status(404);
+
+    const extraData = record.extraData
+      ? JSON.parse(record.extraData.toString())
+      : {};
+      
+    const existingQuestions = extraData.surveyQuestions || [];
+    // extraData.surveyQuestions = [...existingQuestions, ...newQuestions];
+
+    await db
+      .update(programmePhaseSteps)
+      .set({ extraData: JSON.stringify(extraData) })
+      .where(eq(programmePhaseSteps.id, record.id));
+
+    return res.send({ message: "Survey questions saved successfully" });
   });
 
   app.post("/need-analysis/conclude", upload.single("file"), (req, res) => {
