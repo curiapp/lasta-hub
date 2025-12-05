affcda644ce273ee64367af09fed66ebcd9171f3
makosaisaac <59021495+makosaisaac@users.noreply.github.com>
2025-12-05 08:26:14 +0200

    Update environment.prod.ts

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://cd.nust.na',
-  graphqlUrl: 'https://cd.nust.na/graphql'
+  apiUrl: 'https://cd.nust.na/api',
+  graphqlUrl: 'https://cd.nust.na/api/graphql'
 };
